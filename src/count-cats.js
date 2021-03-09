814a34feb922bb3e7190a815cf971f42b1f749f7
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-10 00:16:58 +0300

    init: start js-basic task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+[ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]
+module.exports = function countCats(arr) {
+  let counter = 0;
+  for (let i = 0; i<arr.length; i++) {
+    for (let j = 0; j<arr[i].length; j++) {
+      arr[i][j] === '^^'? counter+=1 : counter = counter
+    }
+  }
+  return counter
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (Array.isArray(arr)) {
+
+  
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) == "string"  ) {
+      arr[i] = arr[i].trim()
+      newArr[i] = arr[i][0];
+      
+    } 
+  }
+  
+  newArr = newArr.sort()
+    /*newArr.push(' ')
+    newArr.unshift(' ')*/
+    newArr = newArr.join('')
+    newArr = newArr.toUpperCase()
+    newArr = newArr.split('').sort().join('')
+    return  newArr
+  } else {
+    return false
+  }
 };
