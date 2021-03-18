de145eb82a10096fc0124c23907c39b0126e0107
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-18 21:21:23 +0300

    check changes

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    arrNew = arr.slice()
+  
+    for (let i = 0; i < arrNew.length; ){
+        
+        if (arrNew[i] === '--double-next' && arrNew[i+1] !== undefined) {
+          arrNew[i] = arrNew[i+1] 
+      } else if (arrNew[i] === '--double-next') {
+        arrNew.splice(i, 1) 
+      } else if (arrNew[i] === '--double-prev' && arrNew[i-1] !== undefined) {
+        arrNew[i] = arrNew[i-1]
+      } else if (arrNew[i] === '--double-prev') {
+        arrNew.splice(i, 1)
+      } else if (arrNew[i] === '--discard-next') {
+        arrNew.splice(i, 1)
+      } else if (arrNew[i] === '--discard-prev' && arrNew[i-1] !== undefined)  {
+        arrNew.splice(i-1, 2)
+      } else if (arrNew[i] === '--discard-prev')  {
+        arrNew.splice(i, 1)
+      }
+      i++
+    }
+    return arrNew
 };
