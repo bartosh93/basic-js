796d27d8ac2d316105374c461393a10ee4d3e2c9
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-29 20:56:14 +0300

    init: start vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  revers = false;
+  constructor(style) {
+    if (style === false) {
+      this.revers = true;
+    }
+  }
+  encrypt(str, keyword) {
+    str = str.toUpperCase();
+    keyword = keyword.toUpperCase();
+    while (keyword.length<str.length) {
+      keyword+=keyword;
+    }
+
+    let strArr = str.split('');
+    let keywordArr = keyword.split('');
+    let darkWord = [];
+    keywordArr.length = strArr.length;
+    strArr = strArr.map(x => x.codePointAt(0));
+    keywordArr = keywordArr.map(x => x.codePointAt(0));
+
+    for (let i = 0, j = 0; i<strArr.length; i++, j++) {
+      if (strArr[i]<65||strArr[i]>90) {
+          darkWord.push(strArr[i]);
+          j = j-1;
+      } else {
+          darkWord.push(((strArr[i] + keywordArr[j])%26) +65);
+      }
+    }
+    darkWord = darkWord.map(x => String.fromCodePoint(x));
+    
+    if (this.revers) darkWord = darkWord.reverse();
+    return darkWord.join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, keyword) {
+    str = str.toUpperCase();
+    keyword = keyword.toUpperCase();
+    while (keyword.length<str.length) {
+      keyword+=keyword;
+    }
+
+    let strArr = str.split('');
+    let keywordArr = keyword.split('');
+    let darkWord = [];
+    keywordArr.length = strArr.length;
+    strArr = strArr.map(x => x.codePointAt(0));
+    keywordArr = keywordArr.map(x => x.codePointAt(0));
+
+    for (let i = 0, j = 0; i<strArr.length; i++, j++) {
+      if (strArr[i]<65||strArr[i]>90) {
+          darkWord.push(strArr[i]);
+          j = j-1;
+      } else {
+          darkWord.push(((strArr[i] - keywordArr[j] +26)%26) +65);
+      }
+    }
+    darkWord = darkWord.map(x => String.fromCodePoint(x));
+    
+    if (this.revers) darkWord = darkWord.reverse();
+    return darkWord.join('');
   }
 }
 
