ce0d9aa40908e19c6014d97829bd656fe1175d43
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-31 01:15:34 +0300

    fix: remove errors

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,53 +2,26 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
    calculateDepth(arr) {
-    /*if (arr.length ==0) {
-      return 1 
-    }    
-      
-    let subArr = []
-    function subCulc(ar) {
-      let deep=1;
-      if (!Array.isArray(ar[0])) {
-        deep = 2
-        return deep
-      } else if (Array.isArray(ar[0])) {
-        return 1 + subCulc(ar[0])
+      let max = 0;
+      if ( Array.isArray(arr) )  {
+          if (max==0) {
+            max +=1  
+          } 
+          let temp = 0;   
+          arr.forEach(el => {
+            if (Array.isArray(el)){
+            let  tempMax = this.calculateDepth(el)
+              if (tempMax>temp){
+                temp = tempMax
+              }
+            }      
+          });
+          max = max + temp
       }
-    }
-    
-     /* for (let i = 0; i < arr.length; i++) {
-        subArr[i] = subCulc(arr[i])
-      }*/
-      
-  /*if (subArr.length !=0) {
-    return Math.max.apply(null, subArr)
-  } else return 1*/
-  
-    let deep;
-    let i = 0;
-      if (!Array.isArray(arr[i])) {
-        deep = 1
-        return deep
-      } else {
-        return 1 + this.calculateDepth(arr[i])
-      }
+      return max
 }
 };
 
 
 
 
-
-
-
-/*calculateDepth(arr) {
-    let deep;
-    let i = 0;
-      if (!Array.isArray(arr[i])) {
-        deep = 1
-        return deep
-      } else {
-        return 1 + this.calculateDepth(arr[i])
-      }
-  }*/
